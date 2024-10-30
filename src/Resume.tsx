a5cec6026329f3246f21836d6c2bdc9f87c3cbca
MATEUSZ EIDING <eidingmateusz@gmail.com>
2024-10-30 01:14:00 +0100

    Create section dir for code split

--- src/Resume.tsx
+++ src/Resume.tsx
@@ -1,33 +1,9 @@
+import ContactInfo from './sections/ContactInfo.section';
+
 export default function Resume() {
     return (
         <>
-            <div className='row'>
-                <div className='col-1 d-flex justify-content-end align-items-center fs-xs'>
-                    <a
-                        href={
-                            'tel:' +
-                            ((import.meta.env.VITE_PHONE as string | undefined)
-                                ?.replaceAll(' ', '')
-                                .replaceAll('-', '') ?? undefined)
-                        }>
-                        {import.meta.env.VITE_PHONE ?? 'undefined'}
-                    </a>
-                </div>
-                <div className='v-divider'></div>
-                <div className='col d-flex justify-content-center uppercase fw-medium fs-sm'>
-                    Mateusz Eiding
-                </div>
-                <div className='v-divider'></div>
-                <div className='col-1 d-flex align-items-center fs-xs'>
-                    <a
-                        href={
-                            'mailto:' +
-                            (import.meta.env.VITE_EMAIL ?? undefined)
-                        }>
-                        {import.meta.env.VITE_EMAIL ?? 'undefined'}
-                    </a>
-                </div>
-            </div>
+            <ContactInfo />
         </>
     );
 }
---
+++ src/sections/ContactInfo.section.tsx
@@ -0,0 +1,27 @@
+export default function ContactInfo() {
+    return (
+        <div className='row'>
+            <div className='col-1 d-flex justify-content-end align-items-center fs-xs'>
+                <a
+                    href={
+                        'tel:' +
+                        ((import.meta.env.VITE_PHONE as string | undefined)
+                            ?.replaceAll(' ', '')
+                            .replaceAll('-', '') ?? undefined)
+                    }>
+                    {import.meta.env.VITE_PHONE ?? 'undefined'}
+                </a>
+            </div>
+            <div className='v-divider'></div>
+            <div className='col d-flex justify-content-center uppercase fw-medium fs-sm'>
+                Mateusz Eiding
+            </div>
+            <div className='v-divider'></div>
+            <div className='col-1 d-flex align-items-center fs-xs'>
+                <a href={'mailto:' + (import.meta.env.VITE_EMAIL ?? undefined)}>
+                    {import.meta.env.VITE_EMAIL ?? 'undefined'}
+                </a>
+            </div>
+        </div>
+    );
+}
