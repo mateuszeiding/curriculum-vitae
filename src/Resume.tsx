43ff37dc5b88bad06260c32bad1b5eb6d41716fa
MATEUSZ EIDING <eidingmateusz@gmail.com>
2024-10-30 01:08:23 +0100

    Contact info

--- src/Resume.tsx
+++ src/Resume.tsx
@@ -2,8 +2,31 @@ export default function Resume() {
     return (
         <>
             <div className='row'>
-                <div className='col-1'>Mateusz Eiding </div>
-                <div className='col-1'>Also Mateusz Eiding </div>
+                <div className='col-1 d-flex justify-content-end align-items-center fs-xs'>
+                    <a
+                        href={
+                            'tel:' +
+                            ((import.meta.env.VITE_PHONE as string | undefined)
+                                ?.replaceAll(' ', '')
+                                .replaceAll('-', '') ?? undefined)
+                        }>
+                        {import.meta.env.VITE_PHONE ?? 'undefined'}
+                    </a>
+                </div>
+                <div className='v-divider'></div>
+                <div className='col d-flex justify-content-center uppercase fw-medium fs-sm'>
+                    Mateusz Eiding
+                </div>
+                <div className='v-divider'></div>
+                <div className='col-1 d-flex align-items-center fs-xs'>
+                    <a
+                        href={
+                            'mailto:' +
+                            (import.meta.env.VITE_EMAIL ?? undefined)
+                        }>
+                        {import.meta.env.VITE_EMAIL ?? 'undefined'}
+                    </a>
+                </div>
             </div>
         </>
     );
