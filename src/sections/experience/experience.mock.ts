55219bbce73997607417448ad3730b17773699c0
MATEUSZ EIDING <eidingmateusz@gmail.com>
2024-11-04 01:46:04 +0100

    Updating experience

--- src/sections/experience/experience.mock.ts
+++ src/sections/experience/experience.mock.ts
@@ -6,27 +6,23 @@ export const experienceData: ExperiencePerPositionProps[] = [
         company: 'TietoEVRY',
         startDate: new Date('2023-08-01'),
         bulletPoints: [
-            `Refactored a React JS solution to align it with design specifications and improve its
-            structure. Applied React Router Data API to simplify maintenance and enhance UI
-            responsiveness.`,
-            `Implemented a new design system in one of the React JS application modules. Converted
-            reusable class components to functional headless components to share business logic across
-            both new and existing UI.`,
-            `Worked on a new design system using web components for framework agnosticism, with
-            customized Bootstrap as the style framework.`,
-            `Joined the team to assist in developing of an application using VueJS, providing frontend
-            development expertise. Proposed and managed the refactoring of two similar APIs
-            working with different data sets, improving their structure and consistency.`,
+            `Refactoring heheh`,
+            `Together with the designers team planned department-wide rebranding. Basing on Figma documentation
+            built libraries that include new design new system as style framework, web components and font-glyphs that were
+            later shared and used by multiple teams.`,
         ],
     },
     {
         position: 'Junior Software Engineer',
         company: 'TietoEVRY',
-        startDate: new Date('2021-09-01'),
-        endDate: new Date('2023-08-01'),
+        startDate: new Date('2021-07-31'),
+        endDate: new Date('2023-09-01'),
         bulletPoints: [
-            `Led the team in enhancing a product acquired from another team, featuring a web
-            frontend in ReactJS and a mobile frontend using Angular with Ionic elements.`,
+            `Transsferred to a new project to provide frontend expertise. Implemented feature for managing
+            deeply nested data structures. Shared knowledge with team members to improve their frontend skills.`,
+            `Took care of frontend in a transferred project. Worked closely with PO and Designer to 
+            provide a cleaner UI and better UX, which contributed to a significant increase 
+            in customer interest.`,
             `Developed a .NET REST API used by the Oslo municipality for seamless data integration
             with external software applications.`,
         ],
@@ -37,8 +33,7 @@ export const experienceData: ExperiencePerPositionProps[] = [
         startDate: new Date('2021-07-01'),
         endDate: new Date('2021-08-31'),
         bulletPoints: [
-            `Mentored interns for two months within a SCRUM team, utilizing VueJS, .NET, and
-            Azure Portal.`,
+            `Worked within the team of 10 interns on internal application for gathering analitycs data.`,
         ],
     },
 ];
