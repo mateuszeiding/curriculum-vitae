0c9d5c27ddc8a61e4e4168395bcc5a9212398520
MATEUSZ EIDING <eidingmateusz@gmail.com>
2024-11-15 03:20:02 +0100

    Deferr skill promises

--- src/sections/skills/!Skills.section.tsx
+++ src/sections/skills/!Skills.section.tsx
@@ -2,8 +2,26 @@ import ResumeAPI from '@/api/Resume.api';
 import Await from '../_shared/Await.component';
 import Default from './Skill.component';
 import { SkillEnum } from './Skill.enum';
+import { useDeferredValue } from 'react';
+import { SkillDto } from '@/models/Skill.dto';
+
+const createSkillPromises = () => {
+    const skillPromises: Record<SkillEnum, Promise<SkillDto>> = Object.create(
+        null
+    );
+
+    Object.keys(SkillEnum).forEach((skillKey: string) => {
+        const key = skillKey as keyof typeof SkillEnum;
+
+        skillPromises[key] = ResumeAPI.getSkills(SkillEnum[key]);
+    });
+
+    return skillPromises;
+};
 
 export default function Skills() {
+    const skills = useDeferredValue(createSkillPromises());
+
     return (
         <section className='row row-gap-5 p-relative'>
             {Object.keys(SkillEnum).map((skillKey: string) => {
@@ -12,7 +30,7 @@ export default function Skills() {
                 return (
                     <Await
                         key={skillKey}
-                        promise={ResumeAPI.getSkills(SkillEnum[key])}
+                        promise={skills[key]}
                         fallback={Default.Skeleton(SkillEnum[key])}
                         resolver={(skill) => (
                             <Default.Component
